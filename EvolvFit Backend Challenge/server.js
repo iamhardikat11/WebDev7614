c22190a6baa72021aaf1c728287c579eb82acc26
iamhardikat11 <iamhardikat11@gmail.com>
2022-07-26 13:46:43 +0530

    commit

--- EvolvFit Backend Challenge/server.js
+++ EvolvFit Backend Challenge/server.js
@@ -6,10 +6,27 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/usersdb',
+const username = "<iamhardikat11>";
+const password = "<Hardik789321>";
+const cluster = "<cluster name>";
+const dbname = "EvolvFitChallengeDB";
+
+mongoose.connect(
+  `mongodb+srv://iamhardikat11:<${password}>@cluster0.4xtqb.mongodb.net/?retryWrites=true&w=majority`, 
   {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
   }
 );
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+app.use(Router);
+
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+});
